14ff3308bb5c2e9c796e82934f93e0900cf36437
PCC <pingchungchang@gmail.com>
2024-09-29 19:23:08 +0800

    added LiChao and fixed CHT

--- 3_Data_Structure/CHT.cpp
+++ 3_Data_Structure/CHT.cpp
@@ -1,45 +1,29 @@
-// dynamic convex hull for dp optimization : line insert & point query
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
+//reference : 8BCube
+#define ll long long
+// only works for integer coordinates!! maintain max
 
-const ll INF = 1ll<<60;
-
-struct Line{
-    mutable ll a, b, r = INF; // y=ax+b, r = upper bound
-    inline Line(ll a, ll b) : a(a), b(b){}
-    inline ll operator()(ll x) const {
-        return a*x+b;
-    }
-    inline bool operator<(const Line &l2) const {
-        return a<l2.a;
-    }
-    inline bool operator<(ll x) const {
-        return r<x;
-    }
+struct Line {
+	mutable ll a, b, p;
+	bool operator<(const Line &rhs) const { return a < rhs.a; }
+	bool operator<(ll x) const { return p < x; }
 };
-
-struct hull : multiset<Line, less<>>{
-    static ll divFloor(ll a, ll b){ // b>=0
-        if(!b) return a>0?INF:-INF;
-        return (a<0?a-b+1:a)/b;
-    }
-    inline bool Intersect(iterator x, iterator y){ // update x->r and return whether *y is killed
-        if(y==end()){
-            x->r = INF;
-            return 0;
-        }
-        return (x->r = divFloor(x->b-y->b, y->a-x->a)) >= y->r;
-    }
-
-    inline void Insert(Line l){
-        auto z = insert(l), y = z++, x = y;
-        while(Intersect(y, z)) z = erase(z); // delete backward
-        if(x!=begin() && Intersect(--x, y)) Intersect(x, y = erase(y)); // delete itself
-        while((y = x)!=begin() && (--x)->r>=y->r) Intersect(x, erase(y)); // delete forward
-    }
-
-    inline ll Query(ll x) const {
-       return (*lower_bound(x))(x);
-    }
+struct CHT : multiset<Line, less<>> {
+	static const ll kInf = 1e18;
+	ll Div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) { x->p = kInf; return 0; }
+		if (x->a == y->a) x->p = x->b > y->b ? kInf : -kInf;
+		else x->p = Div(y->b - x->b, x->a - y->a);
+		return x->p >= y->p;
+	}
+	void addline(ll a, ll b) {
+		auto z = insert({a, b, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p) isect(x, erase(y));
+	}
+	ll query(ll x) {
+		auto l = *lower_bound(x);
+		return l.a * x + l.b;
+	}
 };
