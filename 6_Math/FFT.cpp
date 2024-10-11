823c6a06dbc69fbfef561979c03e55e4b41575c4
PringDeSu <ianneeddiamond@gmail.com>
2024-10-11 15:26:49 +0800

    modified FFT

--- 6_Math/FFT.cpp
+++ 6_Math/FFT.cpp
@@ -1,15 +1,17 @@
-#define FOR(i, j, k) for (int i = j, Z = k; i < Z; i++)
-template <typename T>
+#define TYPE double
+typedef complex<TYPE> cd;
+#undef TYPE
+
 struct FFT {
-    const T pi = acos(-1);
-    complex<T> cis(T theta) {
-        return complex<T>(cos(theta), sin(theta));
+    const double pi = acos(-1);
+    cd cis(double theta) {
+        return cd(cos(theta), sin(theta));
     }
-    complex<T> OMEGA(int n, int k) {
+    cd OMEGA(int n, int k) {
         return cis(pi * 2 * k / n);
     }
-    void apply(complex<T> *a, int N, bool inv) {
-        auto REVERSE = [&](int x) -> int {
+    void operator()(cd *a, int N, bool inv) {
+        auto REV = [&](int x) -> int {
             int ans = 0;
             for (int i = 1; i < N; i <<= 1) {
                 ans <<= 1;
@@ -18,24 +20,24 @@ struct FFT {
             return ans;
         };
         FOR(i, 0, N) {
-            int r = REVERSE(i);
+            int r = REV(i);
             if (i < r) swap(a[i], a[r]);
         }
         for (int w = 1; w < N; w <<= 1) {
-            int omega_n = w << 1;
-            for (int omega_k = 0; omega_k < w; omega_k++) {
-                complex<T> omega = OMEGA(omega_n, (inv ? -1 : 1) * omega_k);
-                for (int s = 0; s < N; s += omega_n) {
-                    complex<T> &L = a[s + omega_k], &R = a[s + omega_k + w];
-                    complex<T> l = L, r = omega * R;
+            int on = w << 1;
+            for (int ok = 0; ok < w; ok++) {
+                cd omega = OMEGA(on, (inv ? -1 : 1) * ok);
+                for (int s = 0; s < N; s += on) {
+                    cd &L = a[s + ok], &R = a[s + ok + w];
+                    cd l = L, r = omega * R;
                     L = l + r;
                     R = l - r;
                 }
             }
         }
         if (inv) {
-            FOR(i, 0, N) a[i] /= N;
+            for (int i = 0; i < N; i++) a[i] /= N;
         }
     }
 };
-#undef FOR
+
